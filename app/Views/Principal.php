141e0dbdf6062da7a980cb968ac1118057472fdf
Jairo <suarezjairo2018@gmail.com>
2020-07-30 20:59:40 -0500

    primer coomit del master original en donde cree la pagina principal y en la carpeta public esta la subcarpeta multimedia y una  hoja de CSS

---
+++ app/Views/Principal.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>boyager's</title>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <img src="img/app.png" width="30" height="30" alt="" loading="lazy">
+        <a class="navbar-brand">KLASKS CELL</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="#">Product</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Register</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="#">Imformation</a>
+              </li>
+          </ul>
+        </div>
+      </nav>
+</head>
+<body>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-12 mt-4">
+                <section class="primera">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                      <div class="carousel-item active">
+                        <img src="img/celu1.jpg" class="d-block w-100" alt="img1">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="img/celu2.jpg" class="d-block w-100" alt="img2">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="img/celu3.jpg" class="d-block w-100" alt="img3">
+                      </div>
+                      <div class="carousel-item">
+                        <img src="img/celu4.jpg" class="d-block w-100" alt="img4">
+                      </div>
+                       <div class="carousel-item">
+                        <img src="img/celu5.jpg" class="d-block w-100" alt="img4">
+                      </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                      <span class="sr-only">Next</span>
+                    </a>
+                  </div>
+                </section>
+                  <div class="mt-3 mb-3">
+                      <h1 class="picturecolor maintext">
+                          KLASKS CELL
+                      </h1>
+                    </div>
+<div>
+    <p>
+        <h3 class="picturecolor secondtext">
+            ¡ the best cell phones you only find in cell klaks !
+        </h3>
+    </p>
+</div>
+<div>
+     <p class="picturecolor tercearytext">
+        Our website offers low-, medium and high-end phones with the best brands in mobile phones such as Apple, samsung, motorola, Huawei, Lenovo, ZTE, Sony, Xiaomi and Nokia come and do not run out of yours with the best promotions at your fingertips without any contracts or plans and you can use it with any operator in any country. We also have good guarantees against defituous damages of our branding marketed, so that you feel safer when buying
+    </p>
+</div>
+<audio src="img/Música Para Negocios Comerciales, Tiendas, Restaurantes, Bares y Cafeterías (2019) - Electronica (128  kbps).mp3" controls autoplay loop></audio>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
